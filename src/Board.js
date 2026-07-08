27dae777225d185fa3781f2c6382c6e19131200d
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-08 17:44:39 +0500

    Completed YC Kanban Board task with Dragula

--- src/Board.js
+++ src/Board.js
@@ -10,9 +10,10 @@ export default class Board extends React.Component {
     const clients = this.getClients();
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
+        // DHYAN SE: Shuru me saare tasks (clients) backlog me jayenge!
+        backlog: clients, 
+        inProgress: [], // Shuru me bilkul khali
+        complete: [],   // Shuru me bilkul khali
       }
     }
     this.swimlanes = {
@@ -21,6 +22,7 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
+
   getClients() {
     return [
       ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
@@ -47,12 +49,59 @@ export default class Board extends React.Component {
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
-      status: companyDetails[3],
+      status: 'backlog', // Sub ka status shuru me backlog kar diya criteria ke mutabiq
     }));
   }
-  renderSwimlane(name, clients, ref) {
+
+  componentDidMount() {
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current,
+    ]);
+
+    this.drake.on('drop', (el, target, source, sibling) => {
+      this.drake.cancel(true);
+
+      const targetStatus = target.getAttribute('data-status');
+      const cardId = el.getAttribute('data-id');
+
+      this.updateCardStatus(cardId, targetStatus);
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.drake) {
+      this.drake.destroy();
+    }
+  }
+
+  updateCardStatus(cardId, targetStatus) {
+    const allClients = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+
+    const updatedClients = allClients.map(client => {
+      if (client.id === cardId) {
+        return { ...client, status: targetStatus };
+      }
+      return client;
+    });
+
+    this.setState({
+      clients: {
+        backlog: updatedClients.filter(client => client.status === 'backlog'),
+        inProgress: updatedClients.filter(client => client.status === 'in-progress'),
+        complete: updatedClients.filter(client => client.status === 'complete'),
+      }
+    });
+  }
+
+  renderSwimlane(name, clients, ref, status) {
     return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
+      <Swimlane name={name} clients={clients} dragulaRef={ref} status={status} />
     );
   }
 
@@ -62,13 +111,13 @@ export default class Board extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
+              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog, 'backlog')}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
+              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress, 'in-progress')}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
+              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete, 'complete')}
             </div>
           </div>
         </div>
